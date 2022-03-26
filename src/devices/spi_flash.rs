dd57785ddf69a91897ae6066305c8c0ab0046efb
Nicolas Viennot <nicolas@viennot.biz>
2022-03-26 06:04:46 -0400

    SPI Flash

--- src/devices/spi_flash.rs
+++ src/devices/spi_flash.rs
@@ -20,18 +20,22 @@ pub struct SpiFlashConfig {
 pub struct SpiFlash {
     pub config: SpiFlashConfig,
     pub content: Vec<u8>,
+    pub read_addr: Option<usize>,
 }
 
 impl TryFrom<SpiFlashConfig> for SpiFlash {
     type Error = anyhow::Error;
 
     fn try_from(config: SpiFlashConfig) -> Result<Self> {
-        let content = util::read_file(&config.file)
+        let mut content = util::read_file(&config.file)
             .with_context(|| format!("Failed to read {}", &config.file))?;
+
+        content.resize(config.size, 0);
 
         Ok(Self {
             config,
             content,
+            read_addr: None,
         })
     }
 }
@@ -42,29 +46,67 @@ impl SpiDevice for SpiFlash {
     }
 
     fn xfer(&mut self, spi: &mut Spi) -> Option<VecDeque<u8>> {
-        let cmd = spi.tx.pop_front().unwrap_or(0xFF);
-        if let Ok(cmd) = Command::try_from(cmd) {
-            debug!("{} cmd={:?}", spi.name, cmd);
-            Some(self.handle_command(cmd, spi))
-        } else if cmd != 0xFF {
-            debug!("{} unknown_cmd={:02x}", spi.name, cmd);
-            None
-        } else {
-            None
-        }
+        spi.tx.front().cloned().map(|cmd| {
+            if cmd == 0xFF {
+                spi.tx.pop_front();
+                self.next_content_read(spi)
+            } else if let Some(cmd) = Command::try_from(cmd).ok() {
+                spi.rx = vec![].into();
+                self.read_addr = None;
+                self.handle_command(cmd, spi)
+            } else {
+                spi.rx = vec![].into();
+                self.read_addr = None;
+                debug!("{} tx={:02x?}", spi.name, spi.tx);
+                None
+            }
+        }).flatten()
     }
 }
 
 impl SpiFlash {
-    fn handle_command(&mut self, cmd: Command, _spi: &mut Spi) -> VecDeque<u8> {
+    fn next_content_read(&mut self, spi: &Spi) -> Option<VecDeque<u8>> {
+        if !spi.rx.is_empty() {
+            return None;
+        }
+
+        self.read_addr.map(|read_addr| {
+            const CHUNK_SIZE: usize = 16;
+            let chunk = &self.content[read_addr..read_addr+CHUNK_SIZE];
+            self.read_addr = Some(read_addr + CHUNK_SIZE);
+            if chunk.is_empty() {
+                None
+            } else {
+                Some(chunk.to_vec().into())
+            }
+        }).flatten()
+    }
+
+    fn handle_command(&mut self, cmd: Command, spi: &mut Spi) -> Option<VecDeque<u8>> {
         match cmd {
             Command::ReadJEDECID => {
+                spi.tx.pop_front();
+                debug!("{} cmd={:?}", spi.name, cmd);
                 let id = self.config.jedec_id;
-                vec![(id >> 16) as u8, (id >> 8) as u8, (id >> 0) as u8].into()
+                Some(vec![(id >> 16) as u8, (id >> 8) as u8, (id >> 0) as u8].into())
             }
-            Command::ReadData => {
-                vec![].into()
+            Command::ReadData if spi.tx.len() == 4 => {
+                spi.tx.pop_front();
+
+                let mut addr = 0;
+
+                while let Some(a) = spi.tx.pop_front() {
+                    addr = (addr << 8) | a as usize;
+                }
+                //let addr = ((addr & 0xFF) << 16) | (addr & 0x00FF00) | (addr >> 16);
+
+                debug!("{} cmd={:?}, addr=0x{:06x}", spi.name, cmd, addr);
+
+                self.read_addr = Some(addr);
+                spi.rx = vec![].into();
+                self.next_content_read(spi)
             }
+            _ => None,
         }
     }
 }
